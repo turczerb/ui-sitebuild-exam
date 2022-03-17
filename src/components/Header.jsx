5a9e7216f4eca53041b72a6039cf2a9068313a14
turczerb <turczerb@gmail.com>
2022-03-17 13:53:21 +0100

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ const Header = (props) => {
   return (
     <div className="header">
       <h1 className="header-h1">New York Times Best Sellers</h1>
+      <span className="material-icons menu-icon">menu</span>
     </div>
   );
 };
